e85b6f52186ac84fc717ed14d4703d08c4ec181e
Punit Tewani <tewanipunit1233@gmail.com>
2022-04-08 18:22:23 +0530

    Initial Commi tPunit

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 import express from 'express'
-import connectDB from './db/connectDB'
+import connectDB from  './db/connectDB.js'
+import dotenv from 'dotenv'
+dotenv.config()
 
 const app = express()
-const port = process.env.PORT || 5000
-const DATABASE_URL = process.env.DATABASE_URL || "mongodb://localhost:27017";
+const port = process.env.PORT
+const DATABASE_URL = process.env.DATABASE_URL;
 
 connectDB(DATABASE_URL);
 
